c62b023218ed5a934113ff0cc03e3ba4a7e85bb1
ferroque-silly <ikindaneedthis@gmail.com>
2025-03-08 13:53:26 -0500

    did some python work

---
+++ python_work/try_yourself_pizzas.py
@@ -0,0 +1,9 @@
+pizzas = ['cheese', 'pepperoni', 'veggie']
+for pizza in pizzas:
+	print(f"I really enjoy to be eating the {pizza.title()}.")
+
+print(f"It's simply insane how much I enjoy the {pizzas[0].title()}.")
+print(f"The {pizzas[1].title()} is quite magnificent.")
+print(f"Let us not forget the humble {pizzas[2].title()}.")
+
+print("I really, really love Pizza!")
