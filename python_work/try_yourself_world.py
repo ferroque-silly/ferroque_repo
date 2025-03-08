427a22b14f84ab4f8c24915b4b586a1ccdc0421d
ferroque-silly <ikindaneedthis@gmail.com>
2025-03-08 12:55:11 -0500

    first commit

---
+++ python_work/try_yourself_world.py
@@ -0,0 +1,23 @@
+places = ['italy', 'philippines', 'germany', 'china', 'mexico']
+
+print(places)
+
+print(sorted(places))
+
+print(places)
+
+print(sorted(places, reverse=True))
+
+print(places)
+
+places.reverse()
+print(places)
+
+places.reverse()
+print(places)
+
+places.sort()
+print(places)
+
+places.sort(reverse=True)
+print(places)
