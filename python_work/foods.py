59b2fb268c38c05c66adc1a37d629565db511c2a
ferroque-silly <ikindaneedthis@gmail.com>
2025-03-09 22:51:37 -0400

    python homework and a script i didn't use

---
+++ python_work/foods.py
@@ -0,0 +1,11 @@
+my_foods = ['pizza', 'falafel', 'carrot cake']
+friend_foods = my_foods[:]
+
+my_foods.append('cannoli')
+friend_foods.append('ice cream')
+
+for food in my_foods:
+	print(food.title())
+
+for food in friend_foods:
+	print(food.title())
---
+++ scripts/touchscreen_off.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+xinput disable "GTCH7503:00 2A94:D009"
