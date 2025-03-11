ac0bc364576145ea8e2544f0d6639ee2e8d0b6df
ferroque-silly <ikindaneedthis@gmail.com>
2025-03-10 23:39:35 -0400

    finishing chapter 4

---
+++ python_work/try_yourself_buffet.py
@@ -0,0 +1,9 @@
+buffet_foods = ('eggs', 'pasta', 'salad', 'toast', 'fish')
+print("Original menu:")
+for buffet_food in buffet_foods:
+	print(buffet_food.title())
+
+buffet_foods = ('eggs', 'pizza', 'salad', 'toast', 'chicken')
+print("\nNew menu:")
+for buffet_food in buffet_foods:
+	print(buffet_food.title())
