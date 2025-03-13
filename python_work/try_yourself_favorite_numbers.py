4ded5e659f649fcedd1b0f31a0e55abd215e1323
ferroque-silly <ikindaneedthis@gmail.com>
2025-03-13 14:17:05 -0400

    some of chapter 6

---
+++ python_work/try_yourself_favorite_numbers.py
@@ -0,0 +1,19 @@
+favorite_numbers = {
+	'drew': 25,
+	'natalia': 24928374,
+	'connor': 3,
+	'kenzie': 8484,
+	'craig': 1000,
+}
+
+drew_number = favorite_numbers['drew']
+natalia_number = favorite_numbers['natalia']
+connor_number = favorite_numbers['connor']
+kenzie_number = favorite_numbers['kenzie']
+craig_number = favorite_numbers['craig']
+
+print(f"Drew's favorite number is {drew_number}.")
+print(f"Natalia's favorite number is {natalia_number}.")
+print(f"Connor's favorite number is {connor_number}.")
+print(f"Kenzie's favorite number is {kenzie_number}.")
+print(f"Craig's favorite number is {craig_number}.")
---
+++ python_work/try_yourself_person.py
@@ -0,0 +1,6 @@
+partner = {'first_name': 'natalia', 'last_name': 'mcdermott', 'age': 28, 'city': 'lowell'}
+
+print(f"My partner's first name is {partner['first_name'].title()}.")
+print(f"My partner's last name is {partner['last_name'].title()}.")
+print(f"My partner's age is {partner['age']}.")
+print(f"My partner lives in {partner['city'].title()}.")
