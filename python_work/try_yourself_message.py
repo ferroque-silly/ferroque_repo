66497169663f877f2508cc8a98823f4e1e0e8b4f
ferroque-silly <ikindaneedthis@gmail.com>
2025-03-23 14:22:03 -0400

    started chapter 8

---
+++ python_work/try_yourself_message.py
@@ -0,0 +1,5 @@
+def display_message():
+	"""Display a message telling everyone what you are learning in this chapter."""
+	print("I'm learning about defining functions in this chapter!")
+
+display_message()
